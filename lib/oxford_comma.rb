a2babbb3dc4dd0fdbcf75068a831296359460939
Luis Leandro <luchrist777@gmail.com>
2020-05-16 00:28:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  return array.first if array.lengh ==1
+  if array.lengh == 2
+     
+     array.insert(1"and").join(" ")
+  elsif array.lengh > 2
+    array[-1].insert(0, "and")
+    array.join(",")
+ end
 end
